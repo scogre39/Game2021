f12b6e265f5bc0d5ba18605d43a6755a5c9c26d6
scogre39 <scogre39@kingshigh.school.nz>
2021-11-08 12:24:13 +1300

    8/11

--- Unnamed Game/Enemy.gd
+++ Unnamed Game/Enemy.gd
@@ -1,25 +1,32 @@
 extends KinematicBody
 
-var player 
+var Character
 var follow_player = false 
 var move_speed = 100 
+var health = 2
 
 func _ready():
 	pass
 	
+func _process(delta):
+	if health <= 0:
+		queue_free()
+	
 func _physics_process(delta):
 	if follow_player == true:
-		var pos = player.global_transform.origin
+		var pos = Character.global_transform.origin
 		var facing = -global_transform.basis.z
 		look_at(pos, Vector3.UP)
-		move_and_slide(facing * move_speed * delta, Vector3.UP)
+		if $RayCast.get_collider() != null:
+			if $RayCast.get_collider().name == "Character":
+				move_and_slide(facing * move_speed * delta, Vector3.UP)
 
 func _on_Area_body_entered(body):
 	if body.name == "Character":
 		$RayCast.set_enabled(true)
 		print("found player")
 		follow_player == true
-		player = body
+		Character = body
 
 
 func _on_Area_body_exited(body):
