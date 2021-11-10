5a6d0321fbbcd98833fe5e070ba49a2c3f031f38
scogre39 <scogre39@kingshigh.school.nz>
2021-11-10 14:59:30 +1300

    10/11

--- Unnamed Game/Enemy.gd
+++ Unnamed Game/Enemy.gd
@@ -1,7 +1,7 @@
 extends KinematicBody
 
-var Character
-var follow_Character = false 
+var character
+var follow_character = false 
 var move_speed = 100 
 var health = 3
 
@@ -13,20 +13,21 @@ func _process(delta):
 		queue_free()
 	
 func _physics_process(delta):
-	if follow_Character == true:
-		var pos = Character.global_transform.origin
+	if follow_character == true:
+		var pos = character.global_transform.origin
 		var facing = -global_transform.basis.z
 		look_at(pos, Vector3.UP)
-		if $RayCast.get_collider() != null:
-			if $RayCast.get_collider().name == "Character":
-				move_and_slide(facing * move_speed * delta, Vector3.UP)
+		move_and_slide(facing * move_speed * delta, Vector3.UP)
+		#if $RayCast.get_collider() != null:
+			#if $RayCast.get_collider().name == "Character":
+				#move_and_slide(facing * move_speed * delta, Vector3.UP)
 
 func _on_Area_body_entered(body):
 	if body.name == "Character":
 		$RayCast.set_enabled(true)
 		print("found player")
-		follow_Character == true
-		Character = body
+		follow_character == true
+		character = body
 		
 
 
@@ -34,5 +35,5 @@ func _on_Area_body_exited(body):
 	if body.name == "Character":
 		$RayCast.set_enabled(false)
 		print("lost player")
-		follow_Character == false
+		follow_character == false
 		
---
+++ Unnamed Game/MainMenu.gd
@@ -0,0 +1,43 @@
+extends MarginContainer
+
+const first_scene = preload("res://Level1.tscn")
+
+onready var selector_one = $CenterContainer/VBoxContainer/CenterContainer2/VBoxContainer/CenterContainer/HBoxContainer/Selector
+onready var selector_two = $CenterContainer/VBoxContainer/CenterContainer2/VBoxContainer/CenterContainer2/HBoxContainer/Selector
+onready var selector_three = $CenterContainer/VBoxContainer/CenterContainer2/VBoxContainer/CenterContainer3/HBoxContainer/Selector
+
+var current_selection = 0
+
+func ready():
+	set_current_selection(0)
+
+func _process(delta):
+	if Input.is_action_just_pressed("move_backward") and current_selection < 2:
+		current_selection += 1
+		set_current_selection(current_selection)
+	elif Input.is_action_just_pressed("move_foward") and current_selection > 0:
+		current_selection -= 1
+		set_current_selection(current_selection)
+	elif Input.is_action_just_pressed("ui_accept"):
+		handle_selection(current_selection)
+
+func handle_selection(_current_selection):
+	if _current_selection == 0:
+		get_parent().add_child(first_scene.instance())
+		queue_free()
+	elif _current_selection == 1:
+		get_parent().add_child(options_menu.instance())
+		queue_free()
+	elif _current_selection == 2:
+		get_tree().quit()
+
+func set_current_selection(_current_selection):
+	selector_one.text = ""
+	selector_two.text = ""
+	selector_three.text = ""
+	if current_selection == 0:
+		selector_1.text = ">"
+	elif current_selection == 1:
+		selector_2.text = ">"
+	elif current_selection == 2:
+		selector_three.text = ">"
